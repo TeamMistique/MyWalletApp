68e14fa9db5d3951ff2973bd04211012b9ed0de5
biaedwards <bibby.georgieva@gmail.com>
2018-07-25 12:46:23 +0300

    Initial commit

---
+++ MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/MyWalletAppApplication.java
@@ -0,0 +1,12 @@
+package com.mistique.mywalletapp.mywalletapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWalletAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWalletAppApplication.class, args);
+    }
+}
---
+++ MyWalletApp/src/test/java/com/mistique/mywalletapp/mywalletapp/MyWalletAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mistique.mywalletapp.mywalletapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyWalletAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
