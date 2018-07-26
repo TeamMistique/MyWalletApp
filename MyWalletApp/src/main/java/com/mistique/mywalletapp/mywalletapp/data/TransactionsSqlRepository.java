50db5146751b10dff82112610a98b67de00d9f19
RadikMih <radik.mihnev@gmail.com>
2018-07-26 16:25:51 +0300

    refactor TypeServiceImpl

--- MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/data/TransactionsSqlRepository.java
+++ MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/data/TransactionsSqlRepository.java
@@ -3,9 +3,11 @@ package com.mistique.mywalletapp.mywalletapp.data;
 import com.mistique.mywalletapp.mywalletapp.data.base.AbstractGenericRepository;
 import com.mistique.mywalletapp.mywalletapp.data.base.GenericRepository;
 import com.mistique.mywalletapp.mywalletapp.models.Transaction;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class TransactionsSqlRepository extends AbstractGenericRepository<Transaction> implements GenericRepository<Transaction> {
 
     @Override
--- MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/services/TypeServiceImpl.java
+++ MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/services/TypeServiceImpl.java
@@ -1,20 +1,26 @@
 package com.mistique.mywalletapp.mywalletapp.services;
 
+import com.mistique.mywalletapp.mywalletapp.data.TypeSqlRepository;
 import com.mistique.mywalletapp.mywalletapp.models.Type;
 import com.mistique.mywalletapp.mywalletapp.services.base.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+
 public class TypeServiceImpl implements TypeService {
+
+    @Autowired
+    private TypeSqlRepository repository;
+
 
     @Override
     public List<Type> getAll() {
-        return null;
+        return repository.listAll();
     }
 
     @Override
     public Type getById(int id) {
-        return null;
+        return repository.findById(id);
     }
 }
