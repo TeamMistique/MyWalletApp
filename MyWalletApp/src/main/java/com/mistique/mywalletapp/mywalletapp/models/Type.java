59e57d1b7e1c922afc7b35a64f93456430c36d3d
RadikMih <radik.mihnev@gmail.com>
2018-07-26 12:23:41 +0300

    Adding type model

--- MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/models/Type.java
+++ MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/models/Type.java
@@ -1,4 +1,39 @@
 package com.mistique.mywalletapp.mywalletapp.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "types")
 public class Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TypeID")
+    private int id;
+
+    @Column(name = "Name")
+    private String name;
+
+    public Type() {
+    }
+
+    public Type(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
