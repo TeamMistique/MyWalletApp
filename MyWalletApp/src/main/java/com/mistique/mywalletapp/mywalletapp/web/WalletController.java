591d956320090b2bc96a5318e74f15fc3f19acb4
biaedwards <bibby.georgieva@gmail.com>
2018-07-26 16:55:08 +0300

    Added wallet controller

--- MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/web/WalletController.java
+++ MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/web/WalletController.java
@@ -1,5 +1,33 @@
 package com.mistique.mywalletapp.mywalletapp.web;
 
-public class WalletController {
+import com.mistique.mywalletapp.mywalletapp.models.Wallet;
+import com.mistique.mywalletapp.mywalletapp.services.base.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/wallets")
+public class WalletController {
+    private WalletService service;
+
+    @Autowired
+    public WalletController(WalletService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/")
+    public List<Wallet> getAll(){
+        return service.listAll();
+    }
+
+    @ExceptionHandler
+    ResponseEntity<WalletsError> handleException(NumberFormatException e) {
+        return new ResponseEntity<>(new WalletsError(HttpStatus.BAD_REQUEST.value(), "Unable to parse employee id."), HttpStatus.BAD_REQUEST);
+    }
 }
