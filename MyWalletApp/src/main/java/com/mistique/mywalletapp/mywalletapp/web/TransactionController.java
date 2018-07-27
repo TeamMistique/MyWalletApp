1ed2dd029bc8e1ab1bc6e38ff3165e4bcdf1ea61
biaedwards <bibby.georgieva@gmail.com>
2018-07-27 10:55:37 +0300

    Some changes over transaction controller

--- MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/web/TransactionController.java
+++ MyWalletApp/src/main/java/com/mistique/mywalletapp/mywalletapp/web/TransactionController.java
@@ -3,24 +3,33 @@ package com.mistique.mywalletapp.mywalletapp.web;
 import com.mistique.mywalletapp.mywalletapp.models.Category;
 import com.mistique.mywalletapp.mywalletapp.models.Transaction;
 import com.mistique.mywalletapp.mywalletapp.models.Wallet;
+import com.mistique.mywalletapp.mywalletapp.services.base.CategoryService;
 import com.mistique.mywalletapp.mywalletapp.services.base.TransactionService;
+import com.mistique.mywalletapp.mywalletapp.services.base.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api/mywallet/transactions")
 public class TransactionController {
     private TransactionService service;
+    private WalletService walletService;
+    private CategoryService categoryService;
 
     @Autowired
-    public TransactionController(TransactionService service) {
+    public TransactionController(TransactionService service, WalletService walletService, CategoryService categoryService) {
         this.service = service;
+        this.walletService = walletService;
+        this.categoryService = categoryService;
     }
 
     @RequestMapping("/")
@@ -34,12 +43,19 @@ public class TransactionController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public void addTransaction(double amount, Date time, Wallet wallet, Category category, String notes) {
-        service.create(amount, time, wallet, category, notes);
+    public void addTransaction(@RequestParam double amount, @RequestParam String time, @RequestParam int walletID,
+                               @RequestParam int categoryID, @RequestParam String notes) throws ParseException {
+        Wallet wallet = walletService.getById(walletID);
+        Category category = categoryService.getById(categoryID);
+        DateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss z yyyy", Locale.ENGLISH);
+        Date date = df.parse(time);
+        service.create(amount, date, wallet, category, notes);
     }
 
     @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
-    public void update(String id, double amount, Date time, Wallet wallet, Category category, String notes) {
+    public void update(@PathVariable("id") String id, @RequestParam(required = false) double amount,
+                       @RequestParam(required = false) Date time, @RequestParam(required = false) int walletID,
+                       @RequestParam(required = false) int categoryID, @RequestParam(required = false) String notes) {
         int idInt = Integer.parseInt(id);
         if (amount != 0) {
             service.update(idInt, amount);
@@ -47,11 +63,11 @@ public class TransactionController {
         if (time != null) {
             service.update(idInt, time);
         }
-        if (wallet != null) {
-            service.update(idInt, wallet);
+        if (walletID != 0) {
+            service.update(idInt, walletService.getById(walletID));
         }
-        if (category != null) {
-            service.update(idInt, category);
+        if (categoryID != 0) {
+            service.update(idInt, categoryService.getById(categoryID));
         }
         if (notes != null) {
             service.update(idInt, notes);
@@ -61,5 +77,10 @@ public class TransactionController {
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable("id") String id) {
         service.delete(Integer.parseInt(id));
+    }
+
+    @ExceptionHandler
+    ResponseEntity<CustomError> handleException(Exception e) {
+        return new ResponseEntity<>(new CustomError(HttpStatus.BAD_REQUEST.value(), "Unable to parse something."), HttpStatus.BAD_REQUEST);
     }
 }
