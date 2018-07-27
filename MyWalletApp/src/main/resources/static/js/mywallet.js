12e96d2020669a5b5094dcd1c11bf360015e66e1
RadikMih <radik.mihnev@gmail.com>
2018-07-27 16:02:15 +0300

    adding side menu in html, css, js

--- MyWalletApp/src/main/resources/static/js/mywallet.js
+++ MyWalletApp/src/main/resources/static/js/mywallet.js
@@ -1,1 +1,9 @@
 //this is java script code :))
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
