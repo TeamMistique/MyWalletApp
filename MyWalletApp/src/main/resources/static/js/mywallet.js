dec6efaa17e155558fbacaae29816429067b10f7
biaedwards <bibby.georgieva@gmail.com>
2018-07-30 12:08:29 +0300

    Added web config file (it might be redundant) and refactored html. Also added an auto fill database-based dropdown for wallets

--- MyWalletApp/src/main/resources/static/js/mywallet.js
+++ MyWalletApp/src/main/resources/static/js/mywallet.js
@@ -7,3 +7,36 @@ function openNav() {
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
 }
+
+var helpers = {
+    buildDropdown: function(result, dropdown, emptyMessage){
+        //Remove current options
+        dropdown.html('');
+
+        //Add the empty option with the empty message
+        dropdown.append('<option value="">'+emptyMessage+'</option>');
+
+        //Check result isn't empty
+        if(result != ''){
+            //Loop through each of the results and append the option to the html
+            $.each(result, function(k,v){
+                
+                dropdown.append('<option value="' + v.id + '">' + v.name + '</option>');
+            });
+        }
+    }
+}
+
+
+$.ajax({
+    type: "POST",
+    url: "/mywallet/wallets/",
+    success: function(data){
+        debugger;
+        helpers.buildDropdown(
+            data,
+            $('#dash-select-wallet'),
+            'Select a wallet'
+        );
+    }
+});
