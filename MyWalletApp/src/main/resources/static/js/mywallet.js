87c9f302bcd0ebd02ca28c2acc9336a4dbfed2af
biaedwards <bibby.georgieva@gmail.com>
2018-08-01 13:16:22 +0300

    Set a date to today for adding new transaction.

--- MyWalletApp/src/main/resources/static/js/mywallet.js
+++ MyWalletApp/src/main/resources/static/js/mywallet.js
@@ -75,3 +75,13 @@ $.ajax({
         );
     }
 });
+
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
+$(function(){
+    $('#select-date').val(new Date().toDateInputValue());
+});
